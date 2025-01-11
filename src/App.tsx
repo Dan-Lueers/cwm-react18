1fcb47559cdd9d25e817bc55d51974d7d7b89798
Dan Lueers <dan.lueers@gmail.com>
2025-01-11 14:25:54 +0100

    added button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import Alert from './components/Alert';
+import Button from './components/Button';
 import ListGroup from './components/ListGroup';
 
 function App() {
   const items = ['New York', 'San Francisco', 'Tokyo', 'Paris', 'Zurich'];
-  const handleSelectItem = (item) => {
+  const handleSelectItem = (item: string) => {
     console.log(item);
+  };
+  const handleClick = () => {
+    console.log('button clicked');
   };
   return (
     <div>
+      <Button color='danger' onClick={handleClick}>
+        Wicked Button
+      </Button>
       <Alert>
         Hello world <p>another parapgrahp of text</p>
         <h2>and some headings</h2>
