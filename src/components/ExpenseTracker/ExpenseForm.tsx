c6b1027b6d7799bc454c350247dfd2e115741a64
Dan-Lueers <dan.lueers@gmail.com>
2025-01-13 15:17:14 +0100

    added form inputs for expense tracker form

--- src/components/ExpenseTracker/ExpenseForm.tsx
+++ src/components/ExpenseTracker/ExpenseForm.tsx
@@ -9,6 +9,23 @@ const ExpenseForm = () => {
           </label>
           <input id="description" type="text" className="form-control" />
         </div>
+        <div className="mb-3">
+          <label htmlFor="amount" className="form-label">
+            Amount
+          </label>
+          <input id="amount" type="number" className="form-control" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="category" className="form-label">
+            Category
+          </label>
+          <select id="category" name="category" className="form-control">
+            <option value=""></option>
+            <option value="groceries">Groceries</option>
+            <option value="utilities">Utilities</option>
+            <option value="entertainment">Entertainment</option>
+          </select>
+        </div>
       </form>
     </>
   );
